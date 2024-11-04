5aaf8bf0c91996e7438523fd02abf2346512051c
Sushanth Sherigar <108352561+Sushanthsush43@users.noreply.github.com>
2024-11-04 13:20:24 +0530

    Update global_variable.dart

--- lib/screens/global/global_variable.dart
+++ lib/screens/global/global_variable.dart
@@ -1,6 +1,6 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 String userName = '';
-String googleMapKey = "AIzaSyB3rZ2NspdLxKSVL5AxEXRbPDQVlbcjyTM";
+String googleMapKey = "";
 CameraPosition GooglePlexInitialPostion = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962), zoom: 14.4746);
